f2e9fcc6ac057f9724c1476d8e71462c0d01bcd3
tidic84 <tidic84@gmail.com>
2021-06-28 23:27:18 +0200

    update

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { readdirSync } = require('fs')
+client.commands = new Discord.Collection();
+
 require('dotenv').config();
 
 require('discord-buttons')(client) ;
 
-client.commands = new Discord.Collection();
+const dir = "commands/"  
+    readdirSync(dir).forEach(dirs => {
+        const commands = readdirSync(`${dir}/${dirs}/`).filter(files => files.endsWith(".js"));
+        for (const file of commands) {
+            const commandFile = require(`${dir}/${dirs}/${file}`);
+            client.commands.set(commandFile.help.name, commandFile);
+            console.log(`Commande chargé --> ${commandFile.help.name}`);
+        }
+    })
 
-// const dir = "./commands/"  
-//     readdirSync(dir).forEach(dirs => {
-//         const commands = readdirSync(`${dir}/${dirs}/`).filter(files => files.endsWith(".js"));
-//         for (const file of commands) {
-//             const commandFile = require(`${dir}/${dirs}/${file}`);
-//             client.commands.set(commandFile.help.name, commandFile);
-//             console.log(`Commande chargé --> ${commandFile.help.name}`)
-//         }
-//     })
 
 ['command_handler', 'event_handler'].forEach(handler => {
     require(`./handlers/${handler}`)(client, Discord);
