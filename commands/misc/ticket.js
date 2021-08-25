48517668ff4a67e4aa699e022de23e79bb392fd5
tidic84 <tidic84@gmail.com>
2021-08-25 20:13:22 +0200

    pleins de truc et des bugs

--- commands/misc/ticket.js
+++ commands/misc/ticket.js
@@ -4,20 +4,24 @@ const { blue, green, yellow, red, purple } = require('../../colors.json')
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 module.exports.run = async (client, message, args) => {
+
     const channel = await message.guild.channels.create(`🎟️・ticket-${message.author.tag}`)
     await channel.setParent('855018849194868751');
-    channel.updateOverwrite(message.author, {
+    await channel.updateOverwrite(message.author, {
         SEND_MESSAGES: true,
         VIEW_CHANNEL: true
     })
+
     const embed = new MessageEmbed()
         .setTitle(`Ticket de ${message.author.tag}`)
-        .setDescription('Merci de contacter le Staff.')
+        .setDescription('Merci d\'avoir contacté le Staff.')
         .setColor(green)
+
     const close = new MessageButton()
         .setStyle("grey")
         .setLabel("🔒 Close")
         .setID("close")
+
     channel.send({ embed: embed, buttons: [close] }).then(m => {
         const filter = (button) => button.clicker.member.hasPermission("ADMINISTRATOR")
         const collector = m.createButtonCollector(filter)
@@ -25,65 +29,153 @@ module.exports.run = async (client, message, args) => {
             b.defer()
             if(b.id === "close") {
                 closeTicket(b.clicker.user)
+                console.log("close")
             }
         })
     })
-    function closeTicket(member) {
-        channel.setName(`🔒・ticket-${message.author.tag}`)
-        channel.updateOverwrite(message.author, {
+
+
+    async function closeTicket(member) {
+        await channel.setName(`🔒・ticket-${message.author.tag}`)
+        await channel.updateOverwrite(message.author, {
             SEND_MESSAGES: false,
             VIEW_CHANNEL: false
         })
         const embed = new MessageEmbed()
-        .setTitle(`Ticket fermé`)
-        .setDescription(`Ce ticket a été fermé par ${member.username}`)
-        .setColor(yellow)
-    const deleteButton = new MessageButton()
-        .setStyle("red")
-        .setLabel("⛔ Delete")
-        .setID("delete")
-    const openButton = new MessageButton()
-        .setStyle("green")
-        .setLabel("🔓 Open")
-        .setID("open")
-    channel.send({ embed: embed, buttons: [openButton, deleteButton] }).then(m => {
-        const filter = (button) => button.clicker.member.hasPermission("ADMINISTRATOR")
-        const collector = m.createButtonCollector(filter, { time: 30000 })
-        collector.on('collect', async b => {
-            b.defer()
-            if(b.id === "delete") {
-                const embed = new MessageEmbed()
-                    .setTitle(`Ticket supprimé`)
-                    .setDescription(`Ce ticket va etre supprimer dans 5 secondes`)
-                    .setColor(red)
-                channel.send({ embed: embed })
+            .setTitle(`Ticket fermé`)
+            .setDescription(`Ce ticket a été fermé par ${member.username}`)
+            .setColor(yellow)
 
-                await delay(5000)
+        const deleteButton = new MessageButton()
+            .setStyle("red")
+            .setLabel("⛔ Delete")
+            .setID("delete")
 
-                channel.delete();
+        const openButton = new MessageButton()
+            .setStyle("green")
+            .setLabel("🔓 Open")
+            .setID("open")
 
-            }
-            else if(b.id === "open") {
-                channel.setName(`🎟️・ticket-${message.author.tag}`)
-                channel.updateOverwrite(message.author, {
+        channel.send({ embed: embed, buttons: [openButton, deleteButton] }).then(m => {
+            const filter = (button) => button.clicker.member.hasPermission("ADMINISTRATOR")
+            const collector = m.createButtonCollector(filter)
+            collector.on('collect', async b => {
+                b.defer()
+
+                if(b.id === "delete") {
+                    const embed = new MessageEmbed()
+                        .setTitle(`Ticket supprimé`)
+                        .setDescription(`Ce ticket va etre supprimer dans 5 secondes`)
+                        .setColor(red)
+                    console.log("delete")
+
+
+                    channel.send({ embed: embed })
+                    await delay(5000)
+                    channel.delete();
+
+            } else if(b.id === "open") {
+                console.log("open")
+
+                await channel.setName(`🎟️・ticket-${message.author.tag}`)
+                await channel.updateOverwrite(message.author, {
                         SEND_MESSAGES: true,
                         VIEW_CHANNEL: true
                 })
-            const embed = new MessageEmbed()
-                .setTitle(`Ticket réouvert`)
-                .setDescription(`Ce ticket a été réouvert par ${b.clicker.user.username}`)
-                .setColor(green)
 
-            channel.send({ embed: embed, buttons: [close] }).then(m => {
-                const filter = (button) => button.clicker.member.hasPermission("ADMINISTRATOR")
-                const collector = m.createButtonCollector(filter, { time: 30000 })
-                collector.on('collect', async b => {
-                    b.defer()
-                    if(b.id === "close") {
-                        closeTicket(b.clicker.user)
-                    }
+                const embed = new MessageEmbed()
+                    .setTitle(`Ticket réouvert`)
+                    .setDescription(`Ce ticket a été réouvert par ${b.clicker.user.username}`)
+                    .setColor(green)
+
+                const closeAgain = new MessageButton()
+                    .setStyle("grey")
+                    .setLabel("🔒 Close")
+                    .setID("closeAgain")
+                channel.send({ embed: embed, buttons: [closeAgain] }).then(m => {
+                    const filter = (button) => button.clicker.member.hasPermission("ADMINISTRATOR")
+                    const collector = m.createButtonCollector(filter)
+                    collector.on('collect', async b => {
+                        b.defer()
+                        if(b.id === "closeAgain") {
+                            console.log("close again")
+                            closeTicket2(b.clicker.user)
+                        }
+                    })
                 })
-            })
+            }
+        })
+    })
+    }
+
+    async function closeTicket2(member) {
+        channel.setName(`🔒・ticket-${message.author.tag}`)
+        await channel.updateOverwrite(message.author, {
+            SEND_MESSAGES: false,
+            VIEW_CHANNEL: false
+        })
+        const embed = new MessageEmbed()
+            .setTitle(`Ticket fermé`)
+            .setDescription(`Ce ticket a été fermé par ${member.username}`)
+            .setColor(yellow)
+
+        const deleteButton = new MessageButton()
+            .setStyle("red")
+            .setLabel("⛔ Delete")
+            .setID("delete")
+
+        const openButton = new MessageButton()
+            .setStyle("green")
+            .setLabel("🔓 Open")
+            .setID("open")
+
+        channel.send({ embed: embed, buttons: [openButton, deleteButton] }).then(m => {
+            const filter = (button) => button.clicker.member.hasPermission("ADMINISTRATOR")
+            const collector = m.createButtonCollector(filter)
+            collector.on('collect', async b => {
+                b.defer()
+
+                if(b.id === "delete") {
+                    const embed = new MessageEmbed()
+                        .setTitle(`Ticket supprimé`)
+                        .setDescription(`Ce ticket va etre supprimer dans 5 secondes`)
+                        .setColor(red)
+                    console.log("delete")
+
+
+                    channel.send({ embed: embed })
+                    await delay(5000)
+                    channel.delete();
+
+            } else if(b.id === "open") {
+                console.log("open")
+
+                await channel.setName(`🎟️・ticket-${message.author.tag}`)
+                await channel.updateOverwrite(message.author, {
+                        SEND_MESSAGES: true,
+                        VIEW_CHANNEL: true
+                })
+
+                const embed = new MessageEmbed()
+                    .setTitle(`Ticket réouvert`)
+                    .setDescription(`Ce ticket a été réouvert par ${b.clicker.user.username}`)
+                    .setColor(green)
+                    
+                const closeAgain2 = new MessageButton()
+                    .setStyle("grey")
+                    .setLabel("🔒 Close")
+                    .setID("closeAgain2")
+                channel.send({ embed: embed, buttons: [closeAgain2] }).then(m => {
+                    const filter = (button) => button.clicker.member.hasPermission("ADMINISTRATOR")
+                    const collector = m.createButtonCollector(filter)
+                    collector.on('collect', async b => {
+                        b.defer()
+                        if(b.id === "closeAgain2") {
+                            console.log("close agin")
+                            closeTicket(b.clicker.user)
+                        }
+                    })
+                })
             }
         })
     })
